21fcb99e5bb392b2211724e8656ded2053662e73
Daniil Demekhin <daniil2002.d11@gmail.com>
2022-09-22 23:53:40 +0300

    ввод переменных

--- GuiStep_0/GuiStep_0.cpp
+++ GuiStep_0/GuiStep_0.cpp
@@ -3,24 +3,47 @@
 #include <conio.h>
 #include "../HelloModule_ru_ru/resource.h"
 #include <locale>
+
 int main()
 {
-	WCHAR userLanguagesMultiString[] = L"ru-RU\0\0";
-	DWORD langCount = 0;
-	HINSTANCE hinstLib;
+    WCHAR userLanguagesMultiString[] = L"ru-RU\0\0";
+    DWORD langCount = 0;
+    HINSTANCE hinstLib;
+    bool info = GetUILanguageInfo(MUI_LANGUAGE_NAME, userLanguagesMultiString, NULL, 0, &langCount);
+    setlocale(LC_ALL, "RUS");
+    
 
-	bool info = GetUILanguageInfo(MUI_LANGUAGE_NAME, userLanguagesMultiString, NULL, 0, &langCount);
-	if (info)
-	{
-		setlocale(LC_ALL, "");
-		hinstLib = LoadLibrary(TEXT("HelloModule_ru_ru.dll"));
-	} else 
-		hinstLib = LoadLibrary(TEXT("HelloModule_en_us.dll"));
-	char qwe[100];
-	LoadStringA(hinstLib, HELLO_MUI_STR_0, qwe, sizeof(qwe));
+    hinstLib = LoadLibrary(TEXT("HelloModule.dll"));
 
-	printf_s("%s", qwe);
-	_getch();
-	return 0;
+    wchar_t* desc = NULL;
+    wchar_t qwe[100];
+    LoadStringW(hinstLib, HELLO_MUI_STR_0, qwe, sizeof(qwe));
+
+    int age;
+    printf_s("\nВведите возраст:\t");
+    scanf_s("%d", &age);
+
+    wchar_t string[15];
+    wprintf_s(L"\nВведите имя:\t");
+    wscanf_s(L"%ls", &string);
+
+    DWORD_PTR pArgs[] = { (DWORD_PTR)string ,
+       (DWORD_PTR)age };
+
+    FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_STRING | FORMAT_MESSAGE_ARGUMENT_ARRAY,
+        qwe,
+        0,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (wchar_t*)&desc,
+        0,
+        (va_list*)pArgs);
+    
+    wprintf(L"%s", desc);
+
+    LocalFree(desc);
+    FreeLibrary(hinstLib);
+
+    _getch();
+    return 0;
 }
 
